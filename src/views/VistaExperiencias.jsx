fe25f76e6591f8d1b61e655eb6b8db59c0efe879
Daniel Bernal <jose.bernal@aviatur.com.co>
2025-07-23 22:05:07 -0500

    Se carga el proyecto inicial

---
+++ src/views/VistaExperiencias.jsx
@@ -0,0 +1,11 @@
+import TabsExperiencias from '../components/TabsExperiencias';
+
+const VistaExperiencias = () => {
+  return (
+    <div className="min-h-screen">
+      <TabsExperiencias />
+    </div>
+  );
+};
+
+export default VistaExperiencias;
