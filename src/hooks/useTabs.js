138f762ae95c0d3e162e01ab3ec6b19087999b7b
Daniel Bernal <jose.bernal@aviatur.com.co>
2025-08-05 23:17:18 -0500

    version 2

--- src/hooks/useTabs.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-const useTabs = () => {
-    const [activeTab, setActiveTab] = useState('Vuelos');
-    return { activeTab, setActiveTab };
-};
-
-export default useTabs;
